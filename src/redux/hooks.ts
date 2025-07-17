edc77a092bc7ba169d4374f7513817a1a29a957a
vikashoperandi <vikash.mishra@operandi.codes>
2025-07-17 17:13:35 +0530

    docs(hooks.ts): add comment to clarify the purpose of useAppDispatch and useAppSelector for type safety

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -2,6 +2,6 @@
 
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from './store';
-
+// Use throughout your app instead of plain `useSelector` `useDispatch` for type safety
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
